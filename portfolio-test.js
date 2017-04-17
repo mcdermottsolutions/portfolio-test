6c7585b32ef7f33b44113b16da919089ecc41134
Mark McDermott <mcdermottsolutions@gmail.com>
2017-04-17 14:32:39 -0500

    taking consoloe log out of callback

--- portfolio-test.js
+++ portfolio-test.js
@@ -4,7 +4,5 @@ var app = express();
 app.get('/',function(req,res) {
   res.send("portfolio test on 8000");
 });
-
-app.listen(8000, function(){
-  console.log('listening on 8000!!');
-});
+app.listen(8000);
+console.log('listening on 8000!!');
